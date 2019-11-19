53afe06a7983733c6dc30ca1a77b625db5ca147d
Ranchino <ranjbahadin@live.se>
2019-11-19 12:32:03 +0100

    added bg-image, fixing css, readme and text

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # A school assignment built with Express and Node.js
-## Uses fetch to make request to two APIs.
+## Uses fetch to make a get requests from two APIs.
+### This app App will show based from the input field information and get weather forecast in celsius and also get information about the capitals country with a flag and population. The default information is Stockholm.
 
 ### To run this app
 1. Run "npm install" to install dependencies
--- public/static/script/script.js
+++ public/static/script/script.js
@@ -44,7 +44,9 @@ function renderWeather(responseData){
     var tempContainer = document.getElementById("temp");
     var cityContainer = document.getElementById("city");
     var countryContainer = document.getElementById("country");
+    var foreCast = document.getElementById("foreCast");
 
+    foreCast.innerHTML = responseData.weather[0].description;
     countryContainer.innerHTML = responseData.sys.country;
     cityContainer.innerHTML = responseData.name;
     tempContainer.innerHTML = Math.round(responseData.main.temp)+ "&#8451";
@@ -63,6 +65,24 @@ function getCityInfo(inputText){
 
 function renderCityInfo(responseData){
     console.log("Info om staden funkar");
+    var populationContainer = document.getElementById("population");
+    var countryName = document.getElementById("countryName");
+    var imageFlag = document.getElementById("imageFlag");
+
+    if(imageFlag.innerHTML.length) {
+        imageFlag.innerHTML = ""
+    }
+    
+    var flag = document.createElement("img")
+    flag.src = responseData[0].flag;
+    flag.style.width.max = "45em";
+    flag.style.height = "25em";
+    flag.style.borderRadius = "8px";
+    imageFlag.appendChild(flag);
+
+    
+    countryName.innerHTML = responseData[0].name;
+    populationContainer.innerHTML = responseData[0].population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 
